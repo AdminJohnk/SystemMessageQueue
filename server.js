134ba3a115af8a43dc4781fb0d761808af16f983
AdminJohnk <truongtantai07042002@gmail.com>
2023-09-22 12:41:45 +0700

    Section 49: Xử lý message khi TTL

--- server.js
+++ server.js
@@ -3,10 +3,32 @@
 const ConsumerService = require('./src/services/consumerQueue.service');
 const queueName = 'test_queue';
 
-ConsumerService.consumerQueue(queueName)
-  .then(() => {
-    console.log(`Message consumer started ${queueName}`);
-  })
-  .catch(err => {
-    console.error(`Message Error: ${err.message}`);
-  });
+const runConsumer = async () => {
+  await ConsumerService.consumerQueue(queueName)
+    .then(() => {
+      console.log(`Message consumer started ${queueName}`);
+    })
+    .catch(err => {
+      console.error(`Message Error: ${err.message}`);
+    });
+
+  await ConsumerService.consumerToQueueNormal(queueName)
+    .then(() => {
+      console.log(`Message consumerToQueueNormal started`);
+    })
+    .catch(err => {
+      console.error(`Message Error: ${err.message}`);
+    });
+
+  await ConsumerService.consumerToQueueFailed(queueName)
+    .then(() => {
+      console.log(`Message consumerToQueueFailed started`);
+    })
+    .catch(err => {
+      console.error(`Message Error: ${err.message}`);
+    });
+
+  console.log('=========================================\n\n');
+};
+
+runConsumer();
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -2,31 +2,91 @@
 
 const { connectToRabbitMQ, closeConnection } = require('../dbs/init.rabbit');
 
+const log = console.log;
+console.log = function () {
+  log.apply(console, [new Date().toISOString()].concat(arguments));
+};
+
 class ConsumerService {
   static async consumerQueue(queueName) {
     try {
       const { channel, connection } = await connectToRabbitMQ();
 
       await channel.assertQueue(queueName, { durable: true });
-      console.log(`Waiting for messages in ${queueName}...`);
       channel.consume(
         queueName,
         message => {
-        //   console.log(
-        //     `Received message: ${queueName}:: ${message.content.toString()}`
-        //   );
-
-        // 1. find User following 
-        // 2. send message to User following
-        // 3. yes, ok ===> success
-        // 4. error, setup DLX ...
-
+          //   console.log(
+          //     `Received message: ${queueName}:: ${message.content.toString()}`
+          //   );
+          // 1. find User following
+          // 2. send message to User following
+          // 3. yes, ok ===> success
+          // 4. error, setup DLX ...
         },
         { noAck: true }
       );
-
     } catch (error) {
       console.error(`Error consuming queue::`, error);
+      throw error;
+    }
+  }
+  // case processing
+  static async consumerToQueueNormal(queueName) {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+      const notiQueue = 'notificationQueueProcess';
+      channel.consume(notiQueue, msg => {
+        // Nghiệp vụ logic
+        console.log(
+          `SEND notificationQueue sucessfully processed:`,
+          msg.content.toString()
+        );
+        channel.ack(msg);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // case failed processing
+  static async consumerToQueueFailed(queueName) {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+
+      const notificationExchangeDLX = 'notificationExDLX'; // notificationEx direct
+      const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX'; // assert
+
+      const notiQueueHandler = 'notificationQueueHotFix';
+      // 1. create Exchange
+      await channel.assertExchange(notificationExchangeDLX, 'direct', {
+        durable: true
+      });
+
+      // 2. create Queue riêng của DLX Exchange
+      const queueResult = await channel.assertQueue(notiQueueHandler, {
+        exclusive: false
+      });
+
+      // 3. bindQueue
+      await channel.bindQueue(
+        queueResult.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+
+      await channel.consume(
+        queueResult.queue,
+        msg => {
+          // Nghiệp vụ logic
+          console.log(
+            `this notification error, pls hotfix:`,
+            msg.content.toString()
+          );
+        },
+        { noAck: true }
+      );
+    } catch (error) {
+      console.error(error);
       throw error;
     }
   }
