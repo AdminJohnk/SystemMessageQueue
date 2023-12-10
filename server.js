06b8e6178abf61f630d60c9758a64913c605bbd6
ZLoganZ <ln26805@gmail.com>
2023-12-10 16:20:50 +0700

    Add HTTP server to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const http = require('http');
 const { connectToMongoDB } = require('./src/database/init.mongodb');
 const ConsumerService = require('./src/services/consumerQueue.service');
 const { arrayClass } = require('./src/models/root.model');
@@ -15,3 +16,12 @@ const runApp = async () => {
 };
 
 runApp();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(process.env.PORT || 12345, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT || 12345}/`);
+});
